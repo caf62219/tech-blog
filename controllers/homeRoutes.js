8b35e48e1a9613b66a35ba62dfd585671101f42c
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-09-15 21:51:48 -0700

    attempted to get the rest of the handlebars working and the routes working but they are not currently working as expected

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -55,7 +55,7 @@ router.get('/', async (req, res) => {
       // Find the logged in user based on the session ID
       const userData = await User.findByPk(req.session.user_id, {
         attributes: { exclude: ['password'] },
-        include: [{ model: Project }],
+        include: [{ model: BlogPost }],
       });
   
       const user = userData.get({ plain: true });
